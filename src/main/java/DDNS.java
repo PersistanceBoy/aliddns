cec8b68bf836fc410bc29dd8876690901aeadcb7
fancy <fancy@influx.io>
2022-02-13 13:01:54 +0800

    备份

---
+++ src/main/java/DDNS.java
@@ -0,0 +1,157 @@
+import com.aliyuncs.DefaultAcsClient;
+import com.aliyuncs.IAcsClient;
+import com.aliyuncs.alidns.model.v20150109.*;
+import com.aliyuncs.exceptions.ClientException;
+import com.aliyuncs.profile.DefaultProfile;
+import com.google.gson.Gson;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.SocketException;
+import java.net.URL;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class DDNS {
+    public static final String host="ljailf520.top";
+
+    public static final String AccessKeyID="LTAI5tPPM4NfTogQjBhPqXL1";
+
+    public static final String AccessKeySecret="FhkD9BjJH91xTgxBksVZLiPEXutHkt";
+    /**
+     * 获取主域名的所有解析记录列表
+     */
+    private DescribeSubDomainRecordsResponse describeSubDomainRecords(DescribeSubDomainRecordsRequest request, IAcsClient client) {
+        try {
+            // 调用SDK发送请求
+            return client.getAcsResponse(request);
+        } catch (ClientException e) {
+            e.printStackTrace();
+            // 发生调用错误，抛出运行时异常
+            throw new RuntimeException();
+        }
+    }
+
+    /**
+     * 修改解析记录
+     */
+    private UpdateDomainRecordResponse updateDomainRecord(UpdateDomainRecordRequest request, IAcsClient client) {
+        try {
+            //  调用SDK发送请求
+            return client.getAcsResponse(request);
+        } catch (ClientException e) {
+            e.printStackTrace();
+            //  发生调用错误，抛出运行时异常
+            throw new RuntimeException();
+        }
+    }
+
+    /**
+     * 修改解析记录
+     */
+    private AddDomainRecordResponse addDomainRecord(AddDomainRecordRequest request, IAcsClient client) {
+        try {
+            //  调用SDK发送请求
+            return client.getAcsResponse(request);
+        } catch (ClientException e) {
+            e.printStackTrace();
+            //  发生调用错误，抛出运行时异常
+            throw new RuntimeException();
+        }
+    }
+
+
+
+    public static void main(String[] args) throws InterruptedException {
+        //  设置鉴权参数，初始化客户端
+        DefaultProfile profile = DefaultProfile.getProfile("cn-hangzhou",// 地域ID
+                AccessKeyID,// 您的AccessKey ID
+                AccessKeySecret);// 您的AccessKey Secret
+        IAcsClient client = new DefaultAcsClient(profile);
+
+        while (true) {
+            try {
+                int i=0;
+                boolean flag=false;
+                while (i<3){
+                    i++;
+                    if(IPv6.ping(host,3)){
+                        flag=true;
+                        break;
+                    }
+                }
+                if(!flag){
+                    LogUtil.logOut("自测地址失败！！！尝试更新ipv6地址");
+                    ddnsOp(client);
+                }
+            } catch (Exception e) {
+                e.printStackTrace();
+                LogUtil.logOut(e);
+            }finally {
+                Thread.sleep(1000 * 30);
+            }
+        }
+    }
+
+    private static void ddnsOp(IAcsClient client) throws SocketException {
+        DDNS ddns = new DDNS();
+
+        //查询指定二级域名的最新解析记录
+        DescribeSubDomainRecordsRequest describeSubDomainRecordsRequest = new DescribeSubDomainRecordsRequest();
+        describeSubDomainRecordsRequest.setSubDomain(host);
+        DescribeSubDomainRecordsResponse describeSubDomainRecordsResponse = ddns.describeSubDomainRecords(describeSubDomainRecordsRequest, client);
+        LogUtil.log_print("describeSubDomainRecords", describeSubDomainRecordsResponse);
+
+        List<DescribeSubDomainRecordsResponse.Record> domainRecords = describeSubDomainRecordsResponse.getDomainRecords();
+        //最新的一条解析记录
+        if (domainRecords.size() != 0) {
+            DescribeSubDomainRecordsResponse.Record record = domainRecords.get(0);
+            //  记录ID
+            String recordId = record.getRecordId();
+            //  记录值
+            String recordsValue = record.getValue();
+            //  当前主机公网IP
+            String currentHostIP = IPv6.getLocalIPv6Address();
+            LogUtil.logOut("-------------------------------当前主机公网IP为：" + currentHostIP + "-------------------------------");
+            if (!currentHostIP.equals(recordsValue)) {
+                //  修改解析记录
+                UpdateDomainRecordRequest updateDomainRecordRequest = new UpdateDomainRecordRequest();
+                //  主机记录
+                updateDomainRecordRequest.setRR("@");
+                //  记录ID
+                updateDomainRecordRequest.setRecordId(recordId);
+                //  将主机记录值改为当前主机IP
+                updateDomainRecordRequest.setValue(currentHostIP);
+                //  解析记录类型
+                updateDomainRecordRequest.setType("AAAA");
+                UpdateDomainRecordResponse updateDomainRecordResponse = ddns.updateDomainRecord(updateDomainRecordRequest, client);
+                LogUtil.log_print("updateDomainRecord", updateDomainRecordResponse);
+            }
+        }else{
+
+            //  当前主机公网IP
+            String currentHostIP = IPv6.getLocalIPv6Address();
+            LogUtil.logOut("-------------------------------当前主机公网IP为：" + currentHostIP + "-------------------------------");
+            AddDomainRecordRequest addDomainRecordRequest=new AddDomainRecordRequest();
+            addDomainRecordRequest.setDomainName(host);
+            addDomainRecordRequest.setRR("@");
+
+//                    //  修改解析记录
+//                    UpdateDomainRecordRequest updateDomainRecordRequest = new UpdateDomainRecordRequest();
+//                    //  主机记录
+//                    updateDomainRecordRequest.setRR("@");
+//                    //  记录ID
+//                    updateDomainRecordRequest.setRecordId(recordId);
+            //  将主机记录值改为当前主机IP
+            addDomainRecordRequest.setValue(currentHostIP);
+            //  解析记录类型
+            addDomainRecordRequest.setType("AAAA");
+            AddDomainRecordResponse updateDomainRecordResponse = ddns.addDomainRecord(addDomainRecordRequest, client);
+            LogUtil.log_print("updateDomainRecord", updateDomainRecordResponse);
+        }
+    }
+}
---
+++ src/main/java/IPv6.java
@@ -0,0 +1,136 @@
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.*;
+import java.util.Enumeration;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * @author liu
+ * @time 2021/11/1
+ * @description
+ */
+public class IPv6 {
+    public static void main(String[] args) throws IOException {
+        /*获取本机所有ip地址(包括保留地址，ipv4,ipv6  如果安装了虚拟机会更多其他的地址)
+         * try {
+            InetAddress ads = null;
+            Enumeration<NetworkInterface>   adds = NetworkInterface.getNetworkInterfaces();
+            while(adds.hasMoreElements()) {
+            Enumeration<InetAddress> inetAds = adds.nextElement().getInetAddresses();
+                while(inetAds.hasMoreElements()) {
+                    ads = inetAds.nextElement();
+                    LogUtil.logOut(ads.getHostAddress());
+                }
+            }
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }*/
+
+        //获取可用ipv6地址
+//        try {
+//            LogUtil.logOut(getLocalIPv6Address());
+//        } catch (SocketException e) {
+//            // TODO Auto-generated catch block
+//            e.printStackTrace();
+//        }
+
+       Boolean l= ping("ljailf520.top", 3);
+       LogUtil.logOut(l);
+
+       //getCheckResult("64 bytes from liujia-ThinkPad-T440s (2409:8a55:3039:68c0::960): icmp_seq=1 TTL=64 time=0.080 ms");
+      //  getCheckResult("16 bytes from 2409:8a55:3039:68c0::960, icmp_seq=13 hlim=55 time=129.993 ms");
+
+
+    }
+    public static String getLocalIPv6Address() throws SocketException {
+        InetAddress inetAddress =null;
+
+        Enumeration<NetworkInterface> networkInterfaces = NetworkInterface.getNetworkInterfaces();
+        outer:
+        while(networkInterfaces.hasMoreElements()) {
+            Enumeration<InetAddress> inetAds = networkInterfaces.nextElement().getInetAddresses();
+            while(inetAds.hasMoreElements()) {
+                inetAddress = inetAds.nextElement();
+                //检查此地址是否是IPv6地址以及是否是保留地址
+                if(inetAddress instanceof Inet6Address&& !isReservedAddr(inetAddress)) {
+                    break outer;
+
+                }
+            }
+        }
+        String ipAddr = inetAddress.getHostAddress();
+        //过滤网卡
+        int index = ipAddr.indexOf('%');
+        if(index>0) {
+            ipAddr = ipAddr.substring(0, index);
+        }
+
+        return ipAddr;
+    }
+    private static boolean isReservedAddr(InetAddress inetAddr) {
+        if(inetAddr.isAnyLocalAddress()||inetAddr.isLinkLocalAddress()||inetAddr.isLoopbackAddress())
+        {
+            return true;
+        }
+        return false;
+    }
+
+    public static boolean ping(String ipAddress, int pingTimes) {
+        BufferedReader in = null;
+        // 将要执行的ping命令,此命令是windows格式的命令
+        Runtime r = Runtime.getRuntime();
+        //String pingCommand = "ping " + ipAddress + " -n " + pingTimes    + " -w " + timeOut;
+        String pingCommand = "ping6 " + ipAddress +" -c " +pingTimes;
+
+        try {   // 执行命令并获取输出
+            LogUtil.logOut(pingCommand);
+            Process p = r.exec(pingCommand);
+            if (p == null) {
+                return false;
+            }
+            // 逐行检查输出,计算类似出现=23ms TTL=62字样的次数
+            in = new BufferedReader(new InputStreamReader(p.getInputStream()));
+            int connectedCount = 0;
+            String line = null;
+            while ((line = in.readLine()) != null) {
+                connectedCount += getCheckResult(line);
+            }   // 如果出现类似=23ms TTL=62这样的字样,出现的次数=测试次数则返回真
+            return connectedCount >= pingTimes;
+        } catch (Exception ex) {
+            ex.printStackTrace();   // 出现异常则返回假
+            return false;
+        } finally {
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    /**
+     * 若line含有=18ms TTL=16字样,说明已经ping通,返回1,否則返回0.
+     */
+    private static int getCheckResult(String line) {
+        LogUtil.logOut("控制台输出的结果为:"+line);
+        String[] lines=line.split("=");
+        String lessStr=lines[lines.length-1].split(" ")[0];
+        try {
+            if(line.contains("Unreachable")){
+                return 0;
+            }
+            if(Double.valueOf(lessStr)>0){
+                return 1;
+            }
+        }catch (Exception e){
+            return 0;
+        }
+        return 0;
+    }
+
+
+}
